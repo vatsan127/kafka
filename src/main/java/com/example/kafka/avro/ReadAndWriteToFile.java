08ac35d253ea35672172e08a436f0883695f1320
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2024-09-06 10:15:12 +0530

    commit for adding the generateAvro readwrite from file

---
+++ src/main/java/com/example/kafka/avro/ReadAndWriteToFile.java
@@ -0,0 +1,48 @@
+package com.example.kafka.avro;
+
+import com.example.Customer;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReadAndWriteToFile {
+
+    private static String FILE_PATH = "src/main/sample/customer-specific.avro";
+
+    public void writeAvroToDataFile(Customer customer) {
+        final DatumWriter<Customer> datumWriter = new SpecificDatumWriter<>(Customer.class);
+
+        try (DataFileWriter<Customer> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(customer.getSchema(), new File(FILE_PATH));
+            dataFileWriter.append(customer);
+            System.out.println("successfully wrote customer-specific.avro");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readAvroDataFromFile() {
+        final File file = new File(FILE_PATH);
+        final DatumReader<Customer> datumReader = new SpecificDatumReader<>(Customer.class);
+        final DataFileReader<Customer> dataFileReader;
+        try {
+            System.out.println("Reading our specific record");
+            dataFileReader = new DataFileReader<>(file, datumReader);
+            while (dataFileReader.hasNext()) {
+                Customer readCustomer = dataFileReader.next();
+                System.out.println(readCustomer.toString());
+                System.out.println("First name: " + readCustomer.getFirstName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
